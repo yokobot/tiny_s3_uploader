43b23b0dc20aa11448c541d1bfb5a9b1f65a2299
yokobot <takuou@gmail.com>
2021-11-14 22:33:30 +0900

    assetsを追加

--- terraform/cognito.tf
+++ terraform/cognito.tf
@@ -8,8 +8,29 @@ resource "aws_cognito_identity_pool" "js" {
 }
 
 resource "aws_iam_role" "unauthenticated_user" {
-  name               = "${var.environment}-tiny-s3-uploader-unauthed-user-role"
-  assume_role_policy = ""
+  name = "${var.environment}-tiny-s3-uploader-unauthed-user-role"
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Federated": "cognito-identity.amazonaws.com"
+      },
+      "Action": "sts:AssumeRoleWithWebIdentity",
+      "Condition": {
+        "StringEquals": {
+          "cognito-identity.amazonaws.com:aud": ${jsonencode(aws_cognito_identity_pool.js.id)}
+        },
+        "ForAnyValue:StringLike": {
+          "cognito-identity.amazonaws.com:amr": "unauthenticated"
+        }
+      }
+    }
+  ]
+}
+  POLICY
 }
 
 resource "aws_iam_role_policy" "unauthenticated_user" {
@@ -17,7 +38,7 @@ resource "aws_iam_role_policy" "unauthenticated_user" {
   role = aws_iam_role.unauthenticated_user.id
 
   policy = jsonencode({
-    Version = 2012 - 10 - 17
+    Version = "2012-10-17"
     Statement = [
       {
         Effect = "Allow"
@@ -30,10 +51,15 @@ resource "aws_iam_role_policy" "unauthenticated_user" {
       {
         Effect = "Allow"
         Action = [
-          "s3:ListBucket"
+          "s3:DeleteObject",
+          "s3:GetObject",
+          "s3:ListBucket",
+          "s3:PutObject",
+          "s3:PutObjectAcl"
         ]
         Resource = [
-          aws_s3_bucket.docs.arn
+          aws_s3_bucket.docs.arn,
+          "${aws_s3_bucket.docs.arn}/*"
         ]
       }
     ]
